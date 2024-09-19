d137cd89b069bde05274d75dc15d185b24a84b60
Jorge Lopez <jorge.lopez@COLPC-HC09C936DY.local>
2024-09-19 17:16:48 -0500

    backend & jenkins file TF

--- EKS-TF/backend.tf
+++ EKS-TF/backend.tf
@@ -1,9 +1,9 @@
 terraform {
   backend "s3" {
-    bucket         = "my-ews-baket1"
+    bucket         = "adostateterraform"
     region         = "us-east-1"
     key            = "End-to-End-Kubernetes-DevSecOps-Tetris-Project/EKS-TF/terraform.tfstate"
-    dynamodb_table = "Lock-Files"
+    dynamodb_table = "terraform-state-lock-dynamo"
     encrypt        = true
   }
   required_version = ">=0.13.0"
